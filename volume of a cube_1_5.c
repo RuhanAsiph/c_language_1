f82cdfa605ac5299ca0c9b108e776ef327f50236
ruhaan-008 <theruhaanasif@gmail.com>
2019-12-17 13:46:19 +0530

    Add files via upload

---
+++ volume of a cube_1_5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/* C program to read a year as an input and find whether it is leap year or not*/
+int main()
+{
+    float side,area;
+    printf("enter side of cube:");
+    scanf("%f",&side);
+
+    area = side*side*side;
+    printf("Volume of cube: %f\n", area);
+    return 0;
+}
