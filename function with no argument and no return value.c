a3753d486db28d91d620f719e343a3ee2fcf158a
ruhaan-008 <theruhaanasif@gmail.com>
2020-01-14 19:20:04 +0530

    Add files via upload

---
+++ function with no argument and no return value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+void sum();
+clrscr();
+sum();
+getch();}
+void sum()
+{
+    int a,b,c;
+    printf("Enter 2 no:\n");
+    scanf("%d%d", &a,&b);
+    c = a+b;
+    printf("%d",c);
+}
